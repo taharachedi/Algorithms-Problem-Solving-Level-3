f749653d02f9ff6d49384ce65626f376fb0cfc77
Taha Rachedi <168340845+taharachedi@users.noreply.github.com>
2024-11-14 16:46:04 +0100

    Create 2 - Sum Each Row in Matrix.cpp

---
+++ 2 - Sum Each Row in Matrix/2 - Sum Each Row in Matrix.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+short Random_Number(short From, short To) {
+    return rand() % (To - From + 1) + From;
+}
+
+
+void Fill_Random_Matrix(short arr[3][3], short Rows, short Cols) {
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = Random_Number(1, 100);
+        }
+    }
+}
+
+
+void Print_Random_Matrix(short arr[3][3], short Rows, short Cols) {
+
+    for (short i = 0; i < Rows; i++) 
+    {
+        for (short j = 0; j < Cols; j++) 
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << endl; 
+    }
+}
+
+
+short Sum_Each_Row(short arr[3][3],short Rows, short Cols) {
+
+    short Sum = 0;
+    
+    for (short j = 0; j < Cols; j++)
+    {
+        Sum += arr[Rows][j];  
+    }
+
+    return Sum; 
+}
+
+
+void Print_Sum_Each_Row(short arr[3][3], short Rows, short Cols) {
+
+    for (short i = 0; i < Rows; i++)
+    {
+        cout << "Row " << i + 1 << " Sum : ";
+        cout << Sum_Each_Row(arr, i, Cols) << endl; 
+    }
+}
+
+
+int main()
+{
+    system("color f4");
+    srand((unsigned)time(NULL));
+
+    short arr[3][3];
+
+    Fill_Random_Matrix(arr, 3, 3);
+
+    cout << "\nThe Following is a 3x3 Random Matrix : \n";
+    Print_Random_Matrix(arr, 3, 3);
+
+    cout << "\nThe Following are The Sum Of Each Row in The Matrix : \n";
+    Print_Sum_Each_Row(arr, 3, 3);
+
+    return 0;
+}
